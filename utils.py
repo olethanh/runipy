cd140707149160e2f67a9168d26a969ef62f9f2d
Laurent Peuch <cortex@worlddomination.be>
2014-01-22 10:27:52 +0100

    [mod] add nazi checks on utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,15 @@ import os
 
 
 def export(name, content):
+    if "/" in name:
+        raise Exception("You aren't able to put a '/' in the filename")
+
+    if not isinstance(name, basestring):
+        raise Exception("The file name should be a string")
+
+    if not isinstance(content, basestring):
+        raise Exception("The file content should be a string")
+
     if "NB_FILES_EXPORT_PATH" not in os.environ:
         return
 
